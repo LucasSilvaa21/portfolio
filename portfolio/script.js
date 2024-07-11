0e6a539f47e0b4fced465d0be25827fb122de306
LucasSilvaa21 <lucasadalbertosilva1@gmail.com>
2024-07-11 17:25:59 -0300

    first commit

---
+++ portfolio/script.js
@@ -0,0 +1,8 @@
+function mostrarSecao() {
+
+    document.querySelector('.main').style.display = 'none';
+    document.getElementById('imagem-inicial').style.display = 'none';
+    document.getElementById('secao-dois').style.display = 'block';
+}
+
+
